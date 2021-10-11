6bb3cc4057315e0380d908658ca7e5c099772d8b
Yeliazar <erik.ruzhakov@mail.ru>
2021-10-11 23:54:41 +0300

    created autherithation, updated middleware

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -6,11 +6,12 @@ class TypeController {
     async create(req, res) {
         const {name} = req.body
         const type = await Type.create({name})
-        return res.json({type})
+        return res.json(type)
     }
 
     async getAll(req, res) {
-
+        const types = await Type.findAll()
+        return res.json(types)
     }
 }
 
