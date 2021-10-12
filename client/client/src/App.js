9600929428c3fafd32d449fb2814fc5ed2840719
Yeliazar <erik.ruzhakov@mail.ru>
2021-10-12 19:23:33 +0300

    created register/login page, created navbar

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import AppRouter from "./components/AppRouter";
+import NavBar from "./components/NavBar";
+import './App.scss';
+
 
 function App() {
   return (
     <BrowserRouter>
-      <AppRouter/>
+        <NavBar/>
+        <AppRouter />
     </BrowserRouter>
   );
 }
--- client/client/src/store/DeviceStore.js
+++ client/client/src/store/DeviceStore.js
@@ -12,7 +12,7 @@ export default class DeviceStore {
             {id: 2, name: "Samsung"},
             {id: 3, name: "Mui"}
         ]
-        this.devices = [
+        this._devices = [
             {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: "https://www.google.com/search?q=iphones&sxsrf=AOaemvK8MU32k16kJArAVevSJO_Q1FE3Xw:1634050610821&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiDms_xkMXzAhX0IMUKHUoIDuoQ_AUoAXoECAEQAw&biw=816&bih=708&dpr=1.25#imgrc=72Po9AU3NiA1gM"},
             {id: 2, name: "Iphone 12 pro", price: 25000, rating: 5, img: "https://www.google.com/search?q=iphones&sxsrf=AOaemvK8MU32k16kJArAVevSJO_Q1FE3Xw:1634050610821&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiDms_xkMXzAhX0IMUKHUoIDuoQ_AUoAXoECAEQAw&biw=816&bih=708&dpr=1.25#imgrc=72Po9AU3NiA1gM"},
             {id: 3, name: "Iphone 12 pro", price: 25000, rating: 5, img: "https://www.google.com/search?q=iphones&sxsrf=AOaemvK8MU32k16kJArAVevSJO_Q1FE3Xw:1634050610821&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiDms_xkMXzAhX0IMUKHUoIDuoQ_AUoAXoECAEQAw&biw=816&bih=708&dpr=1.25#imgrc=72Po9AU3NiA1gM"},
