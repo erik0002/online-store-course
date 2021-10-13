785b487f3070a94bc177ff1e5aecb6f8da2ea5b3
Yeliazar <erik.ruzhakov@mail.ru>
2021-10-13 15:26:29 +0300

    updated DeviceItem, created row with 3 column of Item Device

--- client/client/src/pages/DevicePage.js
+++ client/client/src/pages/DevicePage.js
@@ -1,10 +1,36 @@
 import React from 'react'
-
+import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
+import star from '../asserts/star.png';
 const DevicePage = () => {
+    const device = {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: 'https://t-mobile.scene7.com/is/image/Tmusprod/Apple-iPhone-13-Pink-frontimage'}
     return (
-        <div>
-            DevicePage
-        </div>
+        <Container className={"mt-3"}>
+            <Row>
+                <Col md={4}>
+                    <Image width={300} height={300} src={device.img}/>
+                </Col>
+                <Col md={4}>
+                    <Row className="d-flex flex-column align-items-center">
+                        <h2>{device.name}</h2>
+                        <div
+                            className="d-flex align-items-center justify-content-center"
+                            style={{background: `url(${star}) no-repeat center center`, width: 240, height: 240, backgroundSize: 'cover', fontSize: 50}}
+                        >
+                            {device.rating}
+                        </div>
+                    </Row>
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className="d-flex flex-column align-items-center justify-content-around"
+                        style={{width: 300, height: 300, fontSize: 30, border: '5px solid lightgray'}}
+                    >
+                        <h3>От: {device.price} руб.</h3>
+                        <Button variant={"outline-dark"}>Добавить в корзину</Button>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
