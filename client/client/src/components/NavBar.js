59597d360edf1405a6606eba288da6811a2c6b2e
Yeliazar <erik.ruzhakov@mail.ru>
2021-10-14 10:26:04 +0300

    add description of items to DevicePage, updated NavBar button

--- client/client/src/components/NavBar.js
+++ client/client/src/components/NavBar.js
@@ -2,19 +2,33 @@ import React, {useContext} from 'react';
 import {Context} from "../index";
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
-import {SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
+import {useHistory} from "react-router-dom";
 
 const NavBar = observer(() => {
-    const {user} = useContext(Context)
+    const {user} = useContext(Context);
+    const history = useHistory();
+
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>КупиДевайс</NavLink>
                 {user.isAuth ?
                     <Nav className="ml-auto" style={{color: 'white'}}>
-                        <Button variant={"outline-light"}>Админ панель</Button>
-                        <Button variant={"outline-light"} className="mx-2">Выйти</Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => history.push(ADMIN_ROUTE)}
+                        >
+                            Админ панель
+                        </Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => history.push(LOGIN_ROUTE)}
+                            className="mx-2"
+                        >
+                            Выйти
+                        </Button>
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{color: 'white'}}>
--- client/client/src/pages/DevicePage.js
+++ client/client/src/pages/DevicePage.js
@@ -3,6 +3,13 @@ import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
 import star from '../asserts/star.png';
 const DevicePage = () => {
     const device = {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: 'https://t-mobile.scene7.com/is/image/Tmusprod/Apple-iPhone-13-Pink-frontimage'}
+    const description = [
+        {id: 1, title: 'Оперативная память', description: '5 гб'},
+        {id: 2, title: 'Камера', description: '12 мп'},
+        {id: 3, title: 'Процессор', description: 'Intel core i7'},
+        {id: 4, title: 'Кол-во ядер',  description: '2'},
+        {id: 5, title: 'Аккумулятор', description: '4000'}
+    ]
     return (
         <Container className={"mt-3"}>
             <Row>
@@ -29,6 +36,14 @@ const DevicePage = () => {
                         <Button variant={"outline-dark"}>Добавить в корзину</Button>
                     </Card>
                 </Col>
+            </Row>
+            <Row className="d-flex flex-column m-3">
+                <h1>Характеристики</h1>
+                {description.map((info, index) =>
+                    <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                        {info.title}: {info.description}
+                    </Row>
+                )}
             </Row>
         </Container>
     )
